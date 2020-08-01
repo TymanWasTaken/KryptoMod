9b5ae73d176f7da4c458cdabd3cf94259c4ce4db
Tymanyay <32660892+Tymanyay@users.noreply.github.com>
2020-08-01 16:55:16 -0400

    Start work on json parsing

--- src/main/java/com/tyman/KryptoMod/utils/FetchPlayerDataThread.java
+++ src/main/java/com/tyman/KryptoMod/utils/FetchPlayerDataThread.java
@@ -7,21 +7,35 @@ import net.minecraft.util.EnumChatFormatting;
 public class FetchPlayerDataThread implements Runnable {
 	static ICommandSender iCommandSender = null;
 	static String[] strings;
-	public void run(){  
+	public void run() {
 		iCommandSender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "[KryptoMod] Fetching data for user " + strings[0] + "..."));
-        String response = HttpRequest.get("http://kryptonguildapi.azurewebsites.net/?name="+strings[0]).body();
-        if (response.contains("Error")) {
-        	iCommandSender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "[KryptoMod]" + response));
-        }
-        else {
-        	iCommandSender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "[KryptoMod]" + response));
-        }
+		String response = HttpRequest.get("https://api.hypixel.net/guild?key=0263b520-7895-4f37-84c1-033d04bde642&id=5ef336138ea8c950b6cb73f2").body();
 	}
-	public static void main(ICommandSender iiCommandSender, String[] stringss){
+	public static void main(ICommandSender iiCommandSender, String[] stringss) {
 		iCommandSender = iiCommandSender;
 		strings = stringss;
-	    FetchPlayerDataThread obj=new FetchPlayerDataThread();  
-	    Thread tobj =new Thread(obj);  
-	    tobj.start();
-	} 
+		FetchPlayerDataThread obj = new FetchPlayerDataThread();
+		Thread tobj = new Thread(obj);
+		tobj.start();
+	}
+}
+class Api {
+  private boolean success;
+  private Guild guild;
+  
+  public getSuccess() {
+    return success;
+  }
+  class Guild {
+    private Member[] members;
+
+    public getMembers() {
+      return members;
+    }
+    class Member {
+      private String uuid;
+      private String rank;
+      // Insert gexp here (use map?)
+    }
+  }
 }
